4b31e138f329e3fc3994de055ba68b9b4a34dd2e
Troot0Fobia <andreigorbatov409@gmail.com>
2025-09-14 15:32:00 +0300

    Add cxxopts as a submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <cstdlib>
 #include <iostream>
 
+#include "cxxopts.hpp"
+
 int main(int argc, char* argv[]) {
+    cxxopts::Options options("Renesmee", "Reborn Network Scanner");
+
     std::cout << "Hello" << std::endl;
 
     return EXIT_SUCCESS;
